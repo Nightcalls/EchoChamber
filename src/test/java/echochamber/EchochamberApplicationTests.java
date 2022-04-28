2d24f0349605a10af6ab34d68949266a32151746
Firebat <flamebat@mail.ru>
2022-04-28 23:06:54 +0300

    Add spring-based-project
    Co-Authored-By: supersaxar <vfqkbnnkgjyb@gmail.com>
    Co-Authored-By: Shimmermare <shimmermare@gmail.com>

---
+++ src/test/java/echochamber/EchochamberApplicationTests.java
@@ -0,0 +1,13 @@
+package echochamber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchochamberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
